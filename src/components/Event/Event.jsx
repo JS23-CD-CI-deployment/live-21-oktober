c7397540b8aa298b1c6b837e2d27d577592958b6
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2024-10-21 09:57:55 +0200

    Added code

---
+++ src/components/Event/Event.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
+function Event(props) {
+  const { event } = props;
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/event", { state: { event: event } });
+
+    fetch(`${import.meta.env.VITE_API_URL}/api/events`);
+  }
+
+  return (
+    <article onClick={handleClick}>
+      <h3>{event.name}</h3>
+      <p>{event.where}</p>
+    </article>
+  );
+}
+
+export default Event;
